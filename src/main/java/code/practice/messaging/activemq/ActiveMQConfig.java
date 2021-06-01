100955e77fc10fb375c34e01b5775f592f69065d
Ibrahim Qandeel <iqandeel.c@stc.com.sa>
2021-06-01 12:33:30 +0300

    Create Topic

--- src/main/java/code/practice/messaging/activemq/ActiveMQConfig.java
+++ src/main/java/code/practice/messaging/activemq/ActiveMQConfig.java
@@ -1,8 +1,10 @@
 package code.practice.messaging.activemq;
 
 import javax.jms.Queue;
+import javax.jms.Topic;
 
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class ActiveMQConfig {
     @Bean
     public Queue queue() {
         return new ActiveMQQueue("activeMQ-queue");
+    }
+
+    @Bean
+    public Topic topic() {
+        return new ActiveMQTopic("activeMQ-topic");
     }
 }
